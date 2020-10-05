bf169b0fd3f8bd95b31db8ef65e948ee554057b2
Ruslan Ismayilov <ruslanismayilovv@icloud.com>
2020-10-04 23:31:04 -0700

    fixed simpson pictures

--- Simpson Book/detailsVC.swift
+++ Simpson Book/detailsVC.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class detailsVC: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    var selectedSimpson : Simpson?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        nameLabel.text = selectedSimpson?.name
+        jobLabel.text = selectedSimpson?.job
+        imageView.image = selectedSimpson?.image
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
